2dc0ff80f01280d3e3b3e274907f89dc32a71fff
web-liuyang <984584014@qq.com>
2019-09-25 20:37:15 +0800

    模板文件

---
+++ mysql.js
@@ -0,0 +1,20 @@
+//这里编写数据库信息
+const mysql = require("mysql");
+const defaultOptions = {
+    // 数据库链接
+    host: '127.0.0.1',
+    // 数据库端口
+    port: '3306',
+    // 数据库账号
+    user: 'root',
+    // 数据库密码
+    password: 'root',
+    // 数据库名称
+    database: 'mobile'
+}
+
+function getConnection(options = defaultOptions) {
+    return mysql.createConnection(options);
+}
+//导出数据库信息
+module.exports = getConnection;
